dbc8b15f9c24e8c95cac7fdb3f79d4908afe23d7
MaryamKalantarii <maryamnkalantaria72@gmail.com>
2023-12-16 19:27:29 +0330

    api views

--- config/urls.py
+++ config/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('accounts/',include('django.contrib.auth.urls')),
     path('api-auth/', include('rest_framework.urls')),
+    
 ]
 
 
